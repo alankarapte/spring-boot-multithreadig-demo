05f814e87f9cf6b2095bc643e83c1d98f592dcb4
alankarapte <alankar@vedirasolutions.com>
2021-01-14 21:10:23 +0530

    initial commit

---
+++ src/main/java/com/alankarpractice/executor/api/SpringBootMultithreadigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alankarpractice.executor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultithreadigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultithreadigDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alankarpractice/executor/api/SpringBootMultithreadigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alankarpractice.executor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultithreadigDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
